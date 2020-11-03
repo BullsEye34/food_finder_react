ce323aa9ca88ca3f1548fc093677ef0cb99b760b
Vamshi Prasad <vamvijkum@gmail.com>
2020-11-03 17:54:09 +0530

    Route Js

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,39 @@
-import logo from './logo.svg';
- 
+import {Form, Button, Alert} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+
+const handleBtn = (event) => {
+    
+    <Alert key={0} variant="danger">
+    This is a Danger alert—check it out!
+  </Alert>
+  alert("Yo");
+};
  function Home(){
     return(
         <header className="App-header">
-        <div className="card">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="carda">
+            <div className="content">
+            <Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control size="lg" type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control size="lg" type="password" placeholder="Password" />
+  </Form.Group>
+  <Link to="/result" onClick={handleBtn}>
+  <Button variant="primary" type="submit" onClick="">
+    Submit
+  </Button>
+                                </Link>
+  
+</Form>
+            </div>
         </div>
       </header>
     );
