6f0712b2601d0b685b246a1688ade900f2b6bcc7
Vamshi Prasad <vamvijkum@gmail.com>
2020-11-03 17:54:02 +0530

    Card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Home'
 import { Route } from 'react-router-dom'
 
 function App() {
+  
   return (
     <div className="App">
       <Route exact path="/" component={Home}></Route>
