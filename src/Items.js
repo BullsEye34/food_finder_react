0a68633a4a0d3451bf1abc0cea87cdef252ea22c
Vamshi Prasad <vamvijkum@gmail.com>
2020-11-11 16:56:00 +0530

    Display items and cart

--- src/Items.js
+++ src/Items.js
@@ -5,28 +5,67 @@ import {ListGroup}  from 'react-bootstrap'
 
 export default class Items extends React.Component{
     state={
-        listitems: ["Spring", "Summer", "Fall"]
+        listitems: ["Spring", "Summer", "Fall"], items:[]
     };
     constructor(props){
         super(props)
-       /*  const requestOptions = {
+        this.fetcher();                 
+    }
+    async fetcher (){
+        const requestOptions = {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
           };
-          fetch('http://localhost:3090/auth', requestOptions)
+          await fetch('http://localhost:3090/items', requestOptions)
                       .then(response => response.json())
-                      .then(data => items=data
-                      ); */
+                      .then(data => {
+                          this.setState({items:data})
+                      }
+                      ); 
     }
+    async clicker(thing){
+        console.log(thing)
+    }
+     toBase64(arr) {
+        console.log(arr)
+        //arr = new Uint8Array(arr) //if it's an ArrayBuffer
+        return btoa(
+           arr.reduce((data, byte) => data + String.fromCharCode(byte), '')
+        );
+     }
     render(){
+        console.log(this.state.items)
+        if(this.state.items["data"]===undefined){console.log("Bleh")} else
+        {this.state.items["data"].map(listItem=>(
+            console.log(listItem)
+        ))}
         return(
             <header className="App-header">
       <div className="carda">
 
-<ListGroup>
-{this.state.listitems.map(listitem => (
-    <ListGroup.Item key={listitem}>{listitem}</ListGroup.Item>))}
+<ListGroup className="listt">
+{
+    this.state.items["data"]!==undefined && this.state.items["data"].map(listitem => (
+        <ListGroup.Item className="Itemss" key={listitem.slno} onClick={()=>this.clicker(listitem.name)}>
+            <div className="itemContent">
+            <div className="image">
+                <img className="ItemImg" src={`data:image/png;base64,${this.toBase64(listitem.image["data"])}`}/>
+            </div>
+            <div className="details">
+                <div className="itemName"><h2>{listitem.name}</h2></div>
+                <div className="itemDesc"><h5>{listitem.description}</h5></div>
+                <div className="itemQty"><h3>{listitem.price}</h3></div>
+
+            </div>
+
+            </div>
+        </ListGroup.Item>))
+}
+
 </ListGroup>
+      </div>
+      <div className="cart">
+          <h1>Cart</h1>
       </div>
     </header>
         );
